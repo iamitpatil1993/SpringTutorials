ebc7d416ece357261fc6937f5ebe5808356d649d
Amit Patil <iamitpatil1993@gmail.com>
2018-11-27 22:20:07 +0530

    Chapter-1:Use of @Inject annotation in place of @Autowired annotation

--- src/main/java/autowiring/Driver.java
+++ src/main/java/autowiring/Driver.java
@@ -1,8 +1,22 @@
 package autowiring;
 
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
+// Implementing InitializingBean interface to validate all beans injected correctly. Since this method get called
+// after initialization.
 @Component
-public class Driver {
+public class Driver implements InitializingBean {
+	
+	// We can use @inject annotation from javax.inject in place of @Autowired to inject beans in a
+	// same as we can use @Named in place of @Component
+	@Inject
+	private DrivingLicence drivingLicence;
 
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("Driving licence available ? -> " + drivingLicence != null);
+	}
 }
