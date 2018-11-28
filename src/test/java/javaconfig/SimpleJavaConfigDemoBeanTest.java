54316bd8224c7c7933503d45fa06f5e92040451d
Amit Patil <iamitpatil1993@gmail.com>
2018-11-28 08:30:05 +0530

    Chapter-2:Bean Definition using JavaConfig: Override default bean name using @Bean annotation attributes, multiple bean names

--- src/test/java/javaconfig/SimpleJavaConfigDemoBeanTest.java
+++ src/test/java/javaconfig/SimpleJavaConfigDemoBeanTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
@@ -20,5 +21,12 @@ public class SimpleJavaConfigDemoBeanTest {
 	public void test() {
 		assertNotNull(bean);
 	}
-
+	
+	@Test
+	public void beanIdNameTest() {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(JavaConfiguration.class);
+		assertNotNull(applicationContext.getBean("javaConfigDemoBean"));
+		assertNotNull(applicationContext.getBean("javaConfigDemoBean2"));
+		applicationContext.close();
+	}
 }
