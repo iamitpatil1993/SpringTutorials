a6959cacbb10c3a9ac55ca6d8d40579e898b4a8d
Amit Patil <iamitpatil1993@gmail.com>
2018-11-28 08:18:03 +0530

    Chapter-2:Bean Definition using JavaConfig: Override default bean name using @Bean annotation attributes

--- src/main/java/javaconfig/JavaConfiguration.java
+++ src/main/java/javaconfig/JavaConfiguration.java
@@ -14,8 +14,8 @@ public class JavaConfiguration {
 
 	// @Bean enables discovery of this bean.
 	// Bean type is always return type of method, so it can be interface as well.
-	// Bean Id : method name is a default bean Id for SimpleJavaConfigDemoBean bean.
-	@Bean()
+	// Bean Id : method name is a default bean Id for SimpleJavaConfigDemoBean bean. We can override default name by providing name list/single name to @Bean annotation
+	@Bean(value = {"javaConfigDemoBean", "javaConfigDemoBean2"}) // Note, I provided two names, so bean has one ID and one alias/name. But when I use BeanNameAware to get bean Id it gives me first string as a ID in list.
 	public SimpleJavaConfigDemoBean simpleJavaConfigDemoBean() {
 		// Any logic to instantiate, configure and return bean goes here.
 		// We can do anything to  instantiate, configure, in any way as possiblei in java.
--- src/main/java/javaconfig/SimpleJavaConfigDemoBean.java
+++ src/main/java/javaconfig/SimpleJavaConfigDemoBean.java
@@ -1,10 +1,17 @@
 package javaconfig;
+
+import org.springframework.beans.factory.BeanNameAware;
+
 /**
  * 
  * @author amit
  * This class/bean is just to demo bean declaration using javaConfig
  */
 
-public class SimpleJavaConfigDemoBean {
-	// Nothing to do here for now.
+public class SimpleJavaConfigDemoBean implements BeanNameAware {
+
+	@Override
+	public void setBeanName(String name) {
+		System.out.println("Bean Id for SimpleJavaConfigDemoBean is :: " + name);
+	}
 }
