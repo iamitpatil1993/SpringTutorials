cfdf7855c557801fb172d120c1682c10aa7bcd4a
Amit Patil <iamitpatil1993@gmail.com>
2018-11-29 08:57:47 +0530

    Chapter-2:Explicit Config using XML:Part-3:Constructor-based dependency injection

--- src/main/java/xmlconfig/XmlCDPlayerImpl.java
+++ src/main/java/xmlconfig/XmlCDPlayerImpl.java
@@ -4,14 +4,20 @@
 package xmlconfig;
 
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.InitializingBean;
 
 /**
  * @author amit This bean is used to demonstrate dependency injection using
  *         explicit xml config and assigning custom id to bean in XML config
  */
-public class XmlCDPlayerImpl implements XmlCDPlayer, BeanNameAware {
+public class XmlCDPlayerImpl implements XmlCDPlayer, BeanNameAware, InitializingBean {
 
 	private String id;
+	private XmlConfigCompactDisk compactDisk;
+
+	public XmlCDPlayerImpl(XmlConfigCompactDisk compactDisk) {
+		this.compactDisk = compactDisk;
+	}
 
 	@Override
 	public void setBeanName(String name) {
@@ -21,5 +27,15 @@ public class XmlCDPlayerImpl implements XmlCDPlayer, BeanNameAware {
 	@Override
 	public void play() {
 		System.out.println("Playing music from XmlCDPlayerImpl with bean ID :: " + id);
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		if (compactDisk != null) {
+			System.out.println("XmlCDPlayerImpl is ready to play music");
+			compactDisk.play();
+		} else {
+			System.out.println("Insert compactDisc to play music.");
+		}
 	}
 }
