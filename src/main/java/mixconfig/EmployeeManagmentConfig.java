4a711944e44f51289e5660c583df101e32fe6c8b
Amit Patil <iamitpatil1993@gmail.com>
2018-12-01 16:07:29 +0530

    Chapter-2:refactored code

--- src/main/java/mixconfig/EmployeeManagmentConfig.java
+++ src/main/java/mixconfig/EmployeeManagmentConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @Import(value = { AddressConfig.class, EmployeeConfig.class, ManagerConfig.class })
-@ImportResource(locations = { "classpath:mixconfig/employeeManagmentConfig.xml" }) // importing xml config so that we
+@ImportResource(locations = { "classpath:mixconfig/qualificationConfig.xml" }) // importing xml config so that we
 																					// can use this top level java
 																					// config file with
 																					// AnnotationConfigApplicationContext
