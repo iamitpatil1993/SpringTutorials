d92f3f938d25c3c797cc462ea44933de7e99c9fd
Amit Patil <iamitpatil1993@gmail.com>
2018-12-15 22:11:14 +0530

    Chapter-3: manually add external properties to Environment PropertySources

--- src/main/java/externalizevalues/InjectPropertiesFromExternalDemoBean.java
+++ src/main/java/externalizevalues/InjectPropertiesFromExternalDemoBean.java
@@ -5,15 +5,25 @@ package externalizevalues;
 
 import java.util.Properties;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+
 /**
  * @author amit 
  * This bean demonstrate, how to inject Properties from external
  *         properties file into Properties property of bean
  */
 
-public class InjectPropertiesFromExternalDemoBean {
+public class InjectPropertiesFromExternalDemoBean implements InitializingBean, ApplicationContextAware {
 
 	private Properties externalProperties;
+	private ApplicationContext applicationContext;
 
 	public InjectPropertiesFromExternalDemoBean(Properties externalPropertis) {
 		this.externalProperties = externalPropertis;
@@ -27,5 +37,28 @@ public class InjectPropertiesFromExternalDemoBean {
 		this.externalProperties = externalProperties;
 	}
 
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+	/**
+	 * Here, we are manually adding property source to environment object. 
+	 */
+	@Override
+	public void afterPropertiesSet() throws Exception {
+
+		// 1. ConfigurableEnvironment is used to manipulate Environment object i.e add/update/remove properties also manipulate profiles
+		ConfigurableEnvironment configurableEnvironment = (ConfigurableEnvironment) applicationContext.getEnvironment();
+		
+		// 2. Create appropriate implenetation of PropertySource interface (based on source type properties file or map or system properties or env properties etc.)
+		PropertySource propertiesSource = new PropertiesPropertySource("app.properties", externalProperties);
+		MutablePropertySources mutablePropertySources = configurableEnvironment.getPropertySources();
+		
+		// 3. actually add propertySource to propertySources 
+		mutablePropertySources.addLast(propertiesSource); // Watch we can add propertySource to existing property
+															// sources at first/last or after/before existing, so order
+															// always matters
+	}
 
 }
--- src/test/java/externalizevalues/InjectPropertiesFromExternalDemoBeanTest.java
+++ src/test/java/externalizevalues/InjectPropertiesFromExternalDemoBeanTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.GenericXmlContextLoader;
@@ -25,10 +26,22 @@ public class InjectPropertiesFromExternalDemoBeanTest {
 	@Autowired
 	private InjectPropertiesFromExternalDemoBean bean;
 
+	@Autowired
+	private Environment env;
+
 	@Test
 	public void test() {
 		assertNotNull(bean);
 		assertEquals("Spring Tutorials", bean.getExternalProperties().getProperty("app.name"));
+	}
+
+	/**
+	 * Checks, whether manually added property Source successfully get added to
+	 * environment.
+	 */
+	@Test
+	public void manualExternalPropertySourceAdditionTest() {
+		assertEquals("Spring Tutorials", env.getProperty("app.name"));
 	}
 
 }
